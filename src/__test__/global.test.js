5bfd0fcb356dbc1b7ace6f25d1966d601b21afa6
Gustavo Navarro <genr1818@gmail.com>
2022-04-20 17:54:33 -0500

    Class 3: Testing callbacks, promises and async functions.

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -5,6 +5,13 @@ const reverseString = (str, callback) => {
   callback(str.split("").reverse().join(""))
 };
 
+const reverseString2 = str => {
+  return new Promise((resolve, reject) => {
+    (str)
+      ? resolve(str.split("").reverse().join(""))
+      : reject(Error('Error'));
+  })
+}
 
 test('It must contain a text', () => {
   expect(text).toMatch(/World/);
@@ -22,8 +29,26 @@ test('Truthy', () => {
   expect(true).toBeTruthy();
 })
 
-test('Test a callback', () => {
+test('Test a Callback', () => {
   reverseString('Hello', (str) => {
     expect(str).toBe('olleH');
   })
 })
+
+test('Test a Promise', () => {
+  return reverseString2('Hello')
+    .then(string => {
+      expect(string).toBe('olleH');
+    })
+})
+
+test('Test async/await', async () => {
+  const string = await reverseString2('Hello');
+  expect(string).toBe('olleH');
+})
+
+afterEach(() => console.log('After each test'));
+afterAll(() => console.log('After all the tests'));
+
+beforeEach(() => console.log('Before each test'));
+beforeAll(() => console.log('Before all the tests'));
