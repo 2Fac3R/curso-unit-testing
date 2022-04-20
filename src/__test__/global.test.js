5a12b0ea1b6d73f55d662bfd439cc8c07fd4b909
Gustavo Navarro <genr1818@gmail.com>
2022-04-20 17:43:09 -0500

    Class 2: Basic testing

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,29 @@
 const text = "Hello World!";
+const fruits = ['apple', 'watermelon', 'banana'];
+
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""))
+};
+
 
 test('It must contain a text', () => {
   expect(text).toMatch(/World/);
+})
+
+test('Do we have a banana?', () => {
+  expect(fruits).toContain('banana');
+})
+
+test('Greater than', () => {
+  expect(10).toBeGreaterThan(9);
+})
+
+test('Truthy', () => {
+  expect(true).toBeTruthy();
+})
+
+test('Test a callback', () => {
+  reverseString('Hello', (str) => {
+    expect(str).toBe('olleH');
+  })
 })
